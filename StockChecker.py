093a0add093e0574db732320d42f80ca4957c8ac
joel <joel.hacke@gmail.com>
2020-10-04 16:06:49 -0500

    refactor for oop

--- StockChecker.py
+++ StockChecker.py
@@ -4,13 +4,7 @@ import os
 import configparser
 import time
 from bs4 import BeautifulSoup
-
-BBURL = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440'
-NVIDIAURL = 'https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080'
-NVIDIAAPIURL = 'https://api-prod.nvidia.com/direct-sales-shop/DR/products/en_us/USD/5438481700'
-EVGAURL = 'https://www.evga.com/products/product.aspx?pn=10G-P5-3881-KR'
-
-#"https://in-and-ru-store-api.uk-e1.cloudhub.io/DR/products/en_us/USD/5438481700"
+import json
 
 config = configparser.ConfigParser(interpolation=None)
 configfile = os.path.join(os.path.dirname(__file__), 'config.ini')
@@ -26,15 +20,15 @@ if not os.path.exists(configfile):
 
 config.read(configfile)
 
+with open('sites.json', 'r') as f:
+    sites = json.load(f)
 
-class StockChecker(object):
-    def __init__(self):
-        self.nflag = False
-        self.bflag = False
-        self.napiflag = False
-        self.eflag = False
+class GettingStuff(object):
+    inStockMessage = 'GPU In Stock at {}!'
+    oosMessage = 'Not in stock at {}'
 
-    def getWebpage(self, url, type='text'):
+    def getWebpage(self, type='text'):
+        url = self.url
         headers = {
             "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "accept-encoding": "gzip, deflate, br",
@@ -46,78 +40,53 @@ class StockChecker(object):
         try:
             r = requests.get(url, headers=headers)
             r.raise_for_status()
-            if type=='json':
-                return r.json()
+            if type == 'json':
+                self.request = r.json()
             else:
-                return r.text
+                self.request = r.text
+        # return something that returns the whole thing to an error?
         except requests.exceptions.RequestException as e:
             logger.error('Something went wrong', extra={'error': e})
-            return "Error, Nothing Will Match"
+            self.request = "Error, Nothing Will Match"
         except requests.exceptions as e:
             logger.error('Something went wrong', extra={'error': e})
-            return "Error, Nothing Will Match"
+            self.request = "Error, Nothing Will Match"
 
-    def check_stock(self):
-        #url = 'https://www.bestbuy.com/site/apple-homepod-space-gray/5902410.p?skuId=5902410'
+    def checkStockText(self):
+        inStockMessage = self.inStockMessage
+        oosMessage = self.oosMessage
 
-        bbCards = {
-            'evga': 'https://www.bestbuy.com/site/evga-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card/6432399.p?skuId=6432399',
-            'FE': 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440'
-        }
-
-        pageText = self.getWebpage(BBURL)
-
-        if "/~assets/bby/_img/int/plsvgdef-frontend/svg/cart.svg#cart" in pageText:
-            logger.info('GPU in stock!')
-            return True
+        if self.site == 'API':
+            self.getWebpage(type='json')
         else:
-            logger.info('not in stock')
-            return False
+            self.getWebpage()
 
+        if self.site == 'Nvidia':
+            soup = BeautifulSoup(self.request, 'html.parser')
+            if "Add to Cart" in soup.find_all('div', class_=['cta-button', 'btn', 'show-out-of-stock'])[0]:
+                logger.info(inStockMessage.format(self.site))
+                return True
+            else:
+                logger.info(oosMessage.format(self.site))
+                return False
+        # make it generic? idk
+        if self.site == 'Bestbuy' or self.site == 'Newegg':
+            #if "/~assets/bby/_img/int/plsvgdef-frontend/svg/cart.svg#cart" in self.request:
+            if self.matchTerm in self.request:
+                logger.info(inStockMessage.format(self.site))
+                return True
+            else:
+                logger.info(oosMessage.format(self.site))
+                return False
 
-    def check_evga(self):
-        #url = 'https://www.bestbuy.com/site/apple-homepod-space-gray/5902410.p?skuId=5902410'
-
-
-        evgaurl = 'https://www.bestbuy.com/site/evga-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card/6432399.p?skuId=6432399'
-
-        pageText = self.getWebpage(evgaurl)
-
-        if "/~assets/bby/_img/int/plsvgdef-frontend/svg/cart.svg#cart" in pageText:
-            logger.info('GPU in stock!')
-            return True
         else:
-            logger.info('evga not in stock')
-            return False
+            logger.error('no site somehow!')
 
-
-    def stock_check_nvidia(self):
-        # url = 'https://www.bestbuy.com/site/apple-homepod-space-gray/5902410.p?skuId=5902410'
-        #url = 'https://www.nvidia.com/en-us/geforce/graphics-cards/rtx-2060-super/'
-
-        pageText = self.getWebpage(NVIDIAURL)
-        soup = BeautifulSoup(pageText, 'html.parser')
-
-        #soup.find_all(attrs={'data-theme-override': "null"})[0]['class'] = ['cta-button', 'btn', 'show-out-of-stock']
-
-
-        #if soup.find_all(attrs={'data-title': 'add-to-cart'}):
-        #if not soup.find_all('span', class_='oos-btn'):
-        #if not soup.find_all('div', class_=['cta-button', 'btn', 'show-out-of-stock']):
-        if "Add to Cart" in soup.find_all('div', class_=['cta-button', 'btn', 'show-out-of-stock'])[0]:
-            logger.info('GPU in stock at nvidia!')
-            return True
-        else:
-            logger.info('not in stock at nvidia')
-            return False
-
-    def stock_check_nvidia_api(self):
-
-        pageJSON = self.getWebpage(NVIDIAAPIURL, type='json')
+    def checkStockJSON(self):
+        self.getWebpage()
 
         try:
-            stockStatus = pageJSON['products']['product'][0]['inventoryStatus']['status']
-
+            stockStatus = self.request['products']['product'][0]['inventoryStatus']['status']
             if stockStatus == 'PRODUCT_INVENTORY_IN_STOCK':
                 logger.info('GPU in stock via nvidia API!')
                 return True
@@ -133,6 +102,17 @@ class StockChecker(object):
         except TypeError as e:
             logger.error('Type Error, most likely bad response form api', extra={'error': e})
             return False
+
+
+class GraphicsCard(GettingStuff):
+
+    def __init__(self, url, site, name, matchTerm=None):
+        self.url = url
+        self.site = site
+        self.matchTerm = matchTerm
+        self.name = name
+        self.flag = False
+        self.request = None
 
 
 def telegram_send(message):
@@ -152,99 +132,34 @@ def telegram_send(message):
     return response
 
 
-def isgpuInStockSomewhere(check, flag, url):
-    if check:
-        if not flag:
-            message = 'HOLLY SHIT ITS IN STOCK GO NOW!!!!!!!!!\n' \
-                      '{}'.format(url)
-            response = telegram_send(message)
-
-            if response:
-                logger.info("Message Sent")
-                flag = True
-                logger.info('{} set to true'.format(flag))
-            else:
-                logger.error("Message Not Sent")
-        else:
-            if flag:
-                logger.info('back out of stock, updating {}'.format(flag))
-                flag = False
-
-
 def main():
-    logger.info("Starting!")
-    st = StockChecker()
+    gpuList = []
+    for site in sites:
+        gpuList.append(GraphicsCard(site['url'], site['site'], site['name'], site['match term']))
     while True:
-        if st.check_stock():
-            if not st.bflag:
-                message = 'HOLLY SHIT ITS IN STOCK GO NOW!!!!!!!!!\n' \
-                          'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440'
+        for gpu in gpuList:
+            if gpu.site == 'API':
+                stockStatus = gpu.checkStockJSON()
+            else:
+                stockStatus = gpu.checkStockText()
+
+            # if not in stock and was previously in stock
+            if not stockStatus and gpu.flag:
+                gpu.flag = False
+                logger.info('Flag set to false')
+            # if in stock and wasn't previously in stock
+            elif stockStatus and not gpu.flag:
+                message = 'HOLLY SHIT {} IN STOCK GO NOW!!!!!!!!!\n' \
+                          '{}'.format(gpu.name, gpu.url)
                 response = telegram_send(message)
-
                 if response:
-                    logger.info("Message Sent")
-                    st.bflag = True
-                    logger.info('blfag set to true')
+                    logger.info('Message Sent!')
+                    gpu.flag = True
+                    logger.info('Flat set to true')
                 else:
-                    logger.error("Message Not Sent")
-        else:
-            if st.bflag:
-                logger.info('back out of stock, updating bflag')
-                st.bflag = False
-            #time.sleep(30)
-
-        if st.stock_check_nvidia():
-            if not st.nflag:
-                message = 'HOLLY SHIT ITS IN STOCK GO NOW!!!!!!!!!\n' \
-                          'https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080'
-                response = telegram_send(message)
-
-                if response:
-                    logger.info("Message Sent")
-                    st.nflag = True
-                    logger.info('nlfag set to true')
-                else:
-                    logger.error("Message Not Sent")
-        else:
-            if st.nflag:
-                logger.info('back out of stock, updating nflag')
-                st.nflag = False
-            #time.sleep(30)
-
-        if st.stock_check_nvidia_api():
-            if not st.napiflag:
-                message = 'HOLLY SHIT ITS IN STOCK GO NOW!!!!!!!!!\n' \
-                          'https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080'
-                response = telegram_send(message)
-
-                if response:
-                    logger.info("Message Sent")
-                    st.napiflag = True
-                    logger.info('nlfag set to true')
-                else:
-                    logger.error("Message Not Sent")
-        else:
-            if st.napiflag:
-                logger.info('back out of stock, updating napiflag')
-                st.napiflag = False
-
-        if st.check_evga():
-            if not st.eflag:
-                message = 'HOLLY SHIT ITS IN STOCK GO NOW!!!!!!!!!\n' \
-                          'https://www.bestbuy.com/site/evga-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card/6432399.p?skuId=6432399'
-                response = telegram_send(message)
-
-                if response:
-                    logger.info("Message Sent")
-                    st.eflag = True
-                    logger.info('blfag set to true')
-                else:
-                    logger.error("Message Not Sent")
-        else:
-            if st.eflag:
-                logger.info('back out of stock, updating bflag')
-                st.eflag = False
-
+                    logger.error('Message not Sent')
+            # else is not in stock and wasn't
+            # or is stock and still is
         time.sleep(30)
 
 
